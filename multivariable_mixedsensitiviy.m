c1addffff626c544f3cb50f61c4349aa0666024d
Daan <dagvanhaasteren@gmail.com>
2024-11-20 15:38:46 +0100

    RGA

--- multivariable_mixedsensitiviy.m
+++ multivariable_mixedsensitiviy.m
@@ -1,10 +1,14 @@
 load statespace_data A B C D
 
+w0 = 0;
+w1 = 0.2*3*pi;
+
 % MIMO
 Bm = B(:,1:2);
 Dm = D(:,1:2);
 
 % defining state-space and TF
+s = tf('s');
 
 [b,a] = ss2tf(A, Bm, C(1,:), Dm(1,:),1);
 g11 = tf(b,a);
@@ -18,7 +22,10 @@ g22 = tf(b,a);
 G = [g11 g12;
     g21 g22];
 
+Gw0 = freqresp(G, w0)
+Gw1 = freqresp(G, w1);
 
-RGA = G.*pinv(G).'
+RGAw0 = Gw0.*pinv(Gw0).'
+RGAw1 = Gw1.*pinv(Gw1).'
 
 
