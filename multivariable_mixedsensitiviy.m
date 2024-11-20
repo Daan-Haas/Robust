b21a49118d8f1e9a8dc6c0c1aacb7df6365d0688
Daan <dagvanhaasteren@gmail.com>
2024-11-20 15:27:58 +0100

    Update multivariable_mixedsensitiviy.m

--- multivariable_mixedsensitiviy.m
+++ multivariable_mixedsensitiviy.m
@@ -5,17 +5,20 @@ Bm = B(:,1:2);
 Dm = D(:,1:2);
 
 % defining state-space and TF
-sys = -ss(A, Bm, C, Dm);
 
-[b1,a1] = ss2tf(sys(1,1))
-tf1 = tf(b1, a1)
+[b,a] = ss2tf(A, Bm, C(1,:), Dm(1,:),1);
+g11 = tf(b,a);
+[b, a] = ss2tf(A, Bm, C(1,:), Dm(1,:),2);
+g12 = tf(b,a);
+[b, a] = ss2tf(A, Bm, C(2,:), Dm(2,:),1);
+g21 = tf(b,a);
+[b, a] = ss2tf(A, Bm, C(2,:), Dm(2,:),2);
+g22 = tf(b,a);
+
+G = [g11 g12;
+    g21 g22];
 
 
-
-% computing RGA
-w1 = 0;
-w2 = 0.3*2*pi;
-
-%RGA = G.*inv(G)'
+RGA = G.*pinv(G).'
 
 
